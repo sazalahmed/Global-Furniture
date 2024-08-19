c21cdca24964b1633d025c48d6c24161942ad7b8
freelancersazal <freelancersazal@gmail.com>
2024-08-19 23:38:06 +0600

    Flash sell page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,49 @@ $(function () {
         }
     });
 
+    // Flash Sell Slider
+    $('.flash_sell_slider').slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots: false,
+        arrows: false,
+
+        responsive: [
+            {
+                breakpoint: 1400,
+                settings: {
+                    slidesToShow: 6,
+                }
+            },
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 5,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 4,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 2,
+                }
+            }
+        ]
+    });
+
 
     // Category Slider
     $('.banner_slider').slick({
